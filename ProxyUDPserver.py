9df7b31ef5271bf0e1af994964d36529f16119a7
cemx0121 <105355882+cemx0121@users.noreply.github.com>
2022-12-05 12:18:08 +0100

    Create ProxyUDPserver.py

---
+++ ProxyUDPserver.py
@@ -0,0 +1,25 @@
+import json
+from socket import *
+import threading
+import requests
+
+URL = 'https://lamperestapi.azurewebsites.net/api/lamps'
+def Add(lampInfo):
+    response = requests.post(URL, json=lampInfo)
+    return response
+
+def handleClient(message, clientAddress):
+    print("From IP/Port:", clientAddress)
+    decodedMessage = message.decode()
+    print(decodedMessage)
+    newMessage = json.loads(decodedMessage)
+    response = Add(newMessage)
+    print(response.status_code, response.reason)
+
+serverPort = 18000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(("", serverPort))
+print("The server is ready to receive")
+while True:
+ message, clientAddress = serverSocket.recvfrom(2048)
+ threading.Thread(target=handleClient, args=(message, clientAddress)).start()
