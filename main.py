d6d060462a8ab478e7cf70b1ad14cb1bb190942e
cemx0121 <105355882+cemx0121@users.noreply.github.com>
2022-12-06 10:42:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import RPi.GPIO as GPIO     # Import Library to access GPIO PIN
-import time                 # To access delay function
+import time as t                 # To access delay function
 from GetCityWeatherInfo import *
-from datetime import timedelta
+from datetime import datetime, timedelta
 from UDPsender import *
 
-sunset_time, sunrise_time, visibility, timezone = GetCityWeatherInfo()
+# Collect info about the bulb running this script
+deviceName = input("Angiv hvor navnet på denne pære: ")
+turnOnDuration = input("Angiv hvor længe pæren skal lyse når den fanger bevægelse: ")
+watt = input("Angiv hvor mange watt denne pære bruger: ")
+sunset_time, sunrise_time, visibility, timezone, cityname, country = GetCityWeatherInfo()
 
 PIR_PIN = 7                 # Define PIN for PIR
 Relay_PIN = 11              # Define PIN for Relay
@@ -12,7 +16,6 @@ GPIO.setmode(GPIO.BOARD)    # Consider complete raspberry-pi board
 GPIO.setwarnings(False)     # To avoid same PIN use warning
 GPIO.setup(Relay_PIN,GPIO.OUT)   # Set pin function as output
 GPIO.setup(PIR_PIN,GPIO.IN)   # Set pin function as input
-deviceName = "Phillips Hue Bulb 1" 
 
 try:
     while True:
@@ -20,16 +23,16 @@ try:
         if sunrise_time < timeNow < sunset_time and visibility > 5: # Checks if the time now is within sunrise and sunset
             print("The sun is up and the visibility is above 5km! Motion sensor and lamp OFF! Checking in 1 minute again")
             print("Current time:", timeNow)
-            time.sleep(60)
+            t.sleep(60)
         else:
             if GPIO.input(PIR_PIN) == GPIO.HIGH:
                 print("Motion Detected! Current time:", timeNow)
                 GPIO.output(Relay_PIN, GPIO.LOW) # Relay ON
-                SendUDPPacket(deviceName, timeNow)
-                time.sleep(10)
+                SendUDPPacket(deviceName, timeNow, watt, turnOnDuration, cityname, country)
+                t.sleep(turnOnDuration)
             else:
                 print("No motion detected! Current time:", timeNow)
                 GPIO.output(Relay_PIN, GPIO.HIGH) # Relay OFF
-                time.sleep(1)
+                t.sleep(1)
 except:
     GPIO.cleanup()
