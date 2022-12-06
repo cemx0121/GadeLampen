54d29bfe9007741d0bee820b4fc9cd1722cd6f19
cemx0121 <105355882+cemx0121@users.noreply.github.com>
2022-12-06 11:50:03 +0100

    Update UDPsender.py

--- UDPsender.py
+++ UDPsender.py
@@ -7,6 +7,6 @@ serverPort = 18000
 def SendUDPPacket(deviceName, turnOnTime, watt, turnOnDuration, cityname, country):
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     clientSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-    message = '{"deviceName":"'+ deviceName +'","turnedOn":"'+ turnOnTime +'", "watt": '+ str(watt) +', "turnOnDuration": '+ str(turnOnDuration) +', "city":"'+ cityname +'", "country":"'+ country +'"}'
+    message = '{"deviceName":"'+ str(deviceName) +'","turnedOn":"'+ turnOnTime +'", "watt": '+ str(watt) +', "turnOnDuration": '+ str(turnOnDuration) +', "city":"'+ str(cityname) +'", "country":"'+ str(country) +'"}'
     clientSocket.sendto(message.encode(), (serverName, serverPort))
     clientSocket.close()
