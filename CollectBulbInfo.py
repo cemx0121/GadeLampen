b12cdb5026c0ffe5088582a07a5f1d3c65e2889f
cemx0121 <105355882+cemx0121@users.noreply.github.com>
2022-12-08 12:51:44 +0100

    Update CollectBulbInfo.py
    Tilføjet kommentarer til koden

--- CollectBulbInfo.py
+++ CollectBulbInfo.py
@@ -2,14 +2,17 @@ import requests
 
 url = "https://gadelampenrest.azurewebsites.net/api/Lamps/"
 
+# Collect user info about the bulb running this script
 def CollectBulbInfo():
-    # Collect info about the bulb running this script
+    # Makes HTTP GET Request to our own API and saves the data
     res = requests.get(url)
     data = res.json()
+    # Creates a new array with every devicename from the data
     existingDeviceNames = []
     length = len(data)
     for i in range(length):
         existingDeviceNames.append(data[i]["deviceName"])
+    # Checks if the user input for devicename already exists in the database, if it does error message is printed and user gets to try again.
     while True:
         global deviceName
         deviceName = input("Angiv hvad navnet på denne pære er: ")
@@ -17,6 +20,7 @@ def CollectBulbInfo():
             break
         else:
             print("Et device med dette navn eksistere allerede! Prøv nyt navn!")
+    # Checks if the user input is valid integer, if not user gets to try again
     while True:
         try:
             global turnOnDuration
@@ -24,6 +28,7 @@ def CollectBulbInfo():
             break
         except ValueError:
             print("Indtast venligst et gyldigt input(hel tal)")
+    # Checks if the user input is valid integer, if not user gets to try again
     while True:
         try:
             global watt
